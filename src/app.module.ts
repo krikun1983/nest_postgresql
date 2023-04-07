52ac85c8527bd1385b27d31f52f47529607ef2f3
Stanislav Akhmadeev <stanislav.akhmadeev@idp.zyfra.com>
2023-04-07 20:22:46 +0300

    feat: connected database postgres and install sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nest-course',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
