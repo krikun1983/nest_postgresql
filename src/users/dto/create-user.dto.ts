0aab20cdca2e9c3855c810d0b4da0a96956dd865
Stanislav Akhmadeev <stanislav.akhmadeev@idp.zyfra.com>
2023-04-08 07:49:36 +0300

    feat: add docs swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'user@mail.ru', description: 'Почтовый адрес' })
   readonly email: string;
+
+  @ApiProperty({ example: '123456789', description: 'Пароль пользователя' })
   readonly password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,24 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.models';
 
+@ApiTags('Пользователи')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @ApiOperation({ summary: 'Создание пользователя' })
+  @ApiResponse({ status: 200, type: User })
   @Post()
   createUser(@Body() userDto: CreateUserDto): Promise<User> {
     return this.usersService.createUser(userDto);
   }
 
+  @ApiOperation({ summary: 'Получение пользователей' })
+  @ApiResponse({ status: 200, type: [User] })
   @Get()
   getAllUser(): Promise<User[]> {
     return this.usersService.getAllUsers();
