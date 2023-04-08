8061807ef4f32ada8b26b40eda2491596041d4db
Stanislav Akhmadeev <stanislav.akhmadeev@idp.zyfra.com>
2023-04-08 06:56:08 +0300

    feat: create method createUser and getAllUser

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.models';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Post()
+  createUser(@Body() userDto: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(userDto);
+  }
+
+  @Get()
+  getAllUser(): Promise<User[]> {
+    return this.usersService.getAllUsers();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+
+import { User } from './user.models';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
+  async createUser(dto: CreateUserDto): Promise<User> {
+    return await this.userRepository.create(dto);
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return await this.userRepository.findAll();
+  }
+}
